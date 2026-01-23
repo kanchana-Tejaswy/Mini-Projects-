e087dbfc032c3102d40e81a2733767d20a9bbaff
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2026-01-23 22:37:23 +0530

    writen code for item and price

---
+++ java/Shopping cart program/ShopCart.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ShopCart {
+    
+    public static void main(String[] args) {
+        // Shopping cart program 
+        Scanner scanner = new Scanner(System.in);
+
+        String item ;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+        System.out.println("what item would you like to buy ? :");
+        item = scanner.nextLine();
+
+        System.out.print("What is the price for each ?:");
+        price = scanner.nextDouble();
+
+        System.out.println(price);
+
+        scanner.close();
+    }
+}
